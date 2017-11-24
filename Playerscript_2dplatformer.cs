1c768a812cd3d252cca3a9326a70720e4a5aafc7
adde9708 <adamsegerlund@Adams-MacBook-Air.local>
2017-11-24 11:39:45 +0100

    working scripts

---
+++ Playerscript_2dplatformer.cs
@@ -0,0 +1,160 @@
+﻿/*2D-platformer/datingsim
+Charm
+Avoid objects
+Presents
+Money
+Kiosk were you can spend the money
+Storydriven play
+*/
+//Jump
+// Kill enemies(Jump on them,or use powerup *see below)
+//Power ups X3
+//Mushrooms(more health/bigger)
+//Fireflower(throw damage on enemy)
+//Star()
+
+
+//------------------------
+//My Ideas
+//Endless runner
+//Avoid obstacles(Trees, you bounce back a bit if you hit it)
+
+//Pickup objects
+//Powerups(faster, higher highscore)
+//powerdowns( slower, less highscore, restart, reverse controlls)
+//or use the trees as real obstacles?
+//High score
+//
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerscript_2dplatformer: MonoBehaviour {
+	public GUIText countext;
+	public 	GUIText wintext;
+	private int count;
+	public float speed = 0.3f;
+	public float jump = 1.0f; 
+	public Transform startposition;
+	public int wincount = 4;
+
+	// Use this for initialization
+	void Start () {
+		print ("Hej");
+
+		count = 0;
+		SetCountText ();
+		wintext.text = "";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void FixedUpdate() {
+		PlayerMovement ();
+		QuitApp ();
+	}
+
+public void PlayerMovement () {
+	if (Input.anyKey) {
+
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			//print ("W is pressed");
+			this.transform.Translate (new Vector2 (0, jump));
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			//print ("A is pressed");
+			this.transform.Translate (new Vector2 (-speed, 0));
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			//print ("S is pressed");
+			this.transform.Translate (new Vector2 (0, -speed));
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			//print ("D is pressed");
+			this.transform.Translate (new Vector2 (speed, 0));
+		}
+
+		if (Input.GetKey (KeyCode.R)) {
+			//print ("R is pressed");
+			this.transform.position = startposition.position;
+			this.transform.rotation = startposition.rotation;
+		}
+	 }
+		else
+	{
+		//print ("nothing is pressed");
+	}
+	
+		//print ("Goodbye");
+}
+	void QuitApp() {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+
+		}
+	}
+	//On Collision something something 
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "wall") {
+		
+			print ("Collision has been dectected");
+	
+		}
+		if (collision.gameObject.tag == "Checkpoint") {
+			print ("Checkpoint has been dectected");       	
+		}
+		if (collision.gameObject.tag == "start") {
+			print ("start has been dectected");
+		}
+		if (collision.gameObject.tag == "end") {
+			print ("end has been dectected");
+
+		}
+	
+	}
+	void OnTriggerStay(Collider trigger) {
+		if (trigger.gameObject.tag == "Ladder") 
+		{
+			print ("Ladder is enabled.");
+		
+		}
+}
+
+	void OnTriggerEnter(Collider other) {	
+		
+		if(other.gameObject.tag == "Pickup")
+		{  
+			print ("Picked up object");
+		 other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+	  }
+		if (other.gameObject.tag == "Power Up") 
+		{
+			print ("powerup grabbed!");
+		
+		}
+
+	}
+	void SetCountText() {
+
+		countext.text="Count" + count.ToString();
+		if(count >=wincount) {
+		wintext.text="You win!";
+		Application.LoadLevel("level02");
+
+		}
+
+	}
+}
---
+++ Playerscript_topview.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerscript_topview: MonoBehaviour {
+
+	public float speed = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+		print ("Hej");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.anyKey) {
+
+
+			if (Input.GetKey (KeyCode.W)) {
+				print ("W is pressed");
+				this.transform.Translate (new Vector2 (0, speed));
+			}
+
+			if (Input.GetKey (KeyCode.A)) {
+				print ("A is pressed");
+				this.transform.Translate (new Vector2 (-speed, 0));
+			}
+
+			if (Input.GetKey (KeyCode.S)) {
+				print ("S is pressed");
+				this.transform.Translate (new Vector2 (0, -speed));
+			}
+
+			if (Input.GetKey (KeyCode.D)) {
+				print ("D is pressed");
+				this.transform.Translate (new Vector2 (speed, 0));
+			}
+
+		} else {
+			print ("nothing is pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+			print ("Goodbye");
+	}
+}
