385da9a0a511725bee94e8070fdc19a1d32c3ef1
adde9708 <adamsegerlund@Adams-MacBook-Air.local>
2017-11-28 12:15:27 +0100

    Formatting fixes plus working speedboost

--- Playerscript_2dplatformer.cs
+++ Playerscript_2dplatformer.cs
@@ -1,14 +1,13 @@
 ﻿//------------------------
-//My Ideas
-//Endless runner
-//Avoid obstacles(Trees, you bounce back a bit if you hit it)
-//Pickup objects
-//Powerups(faster, higher highscore)
-//powerdowns( slower, less highscore, restart, reverse controlls)
-//or use the trees as real obstacles?
-//High score
+// My Ideas
+// Endless runner
+// Avoid obstacles(Trees, you bounce back a bit if you hit it)
+// Pickup objects
+// Powerups(faster, higher highscore)
+// powerdowns( slower, less highscore, restart, reverse controlls)
+// or use the trees as real obstacles?
+// High score
 //
-
 
 using System.Collections;
 using System.Collections.Generic;
@@ -23,129 +22,112 @@ public class Playerscript_2dplatformer: MonoBehaviour {
 	public Transform startposition;
 	public int wincount = 4;
 
-	// Use this for initialization
-	void Start () {
-		print ("Hej");
-		count = 0;
-		SetCountText ();
-		wintext.text = "";
+          // Use this for initialization
+          void Start() {
+            print("Hej");
+            count = 0;
+            SetCountText();
+            wintext.text = "";
+          }
 
-	}
+          // Update is called once per frame
+          void Update() {
 
-	// Update is called once per frame
-	void Update () {
+					}
 
+          void FixedUpdate() {
+            PlayerMovement();
+            QuitApp();
+          }
 
-	}
+        public
+          void PlayerMovement() {
+            if (Input.anyKey) {
 
-	void FixedUpdate() {
-		PlayerMovement ();
-		QuitApp ();
-	}
+              if (Input.GetKeyDown(KeyCode.W)) {
+                // print ("W is pressed");
+                this.transform.Translate(new Vector2(0, jump));
+              }
 
-public void PlayerMovement () {
-	if (Input.anyKey) {
+              if (Input.GetKey(KeyCode.A)) {
+                // print ("A is pressed");
+                this.transform.Translate(new Vector2(-speed, 0));
+              }
 
-		if (Input.GetKeyDown (KeyCode.W)) {
-			//print ("W is pressed");
-			this.transform.Translate (new Vector2 (0, jump));
-		}
+              if (Input.GetKey(KeyCode.S)) {
+                // print ("S is pressed");
+                this.transform.Translate(new Vector2(0, -speed));
+              }
 
-		if (Input.GetKey (KeyCode.A)) {
-			//print ("A is pressed");
-			this.transform.Translate (new Vector2 (-speed, 0));
-		}
+              if (Input.GetKey(KeyCode.D)) {
+                // print ("D is pressed");
+                this.transform.Translate(new Vector2(speed, 0));
+              }
 
-		if (Input.GetKey (KeyCode.S)) {
-			//print ("S is pressed");
-			this.transform.Translate (new Vector2 (0, -speed));
-		}
+              if (Input.GetKey(KeyCode.R)) {
+                // print ("R is pressed");
+                this.transform.position = startposition.position;
+                this.transform.rotation = startposition.rotation;
+              }
+            } else {
+              // print ("nothing is pressed");
+            }
 
-		if (Input.GetKey (KeyCode.D)) {
-			//print ("D is pressed");
-			this.transform.Translate (new Vector2 (speed, 0));
-		}
+            // print ("Goodbye");
+          }
+          void QuitApp() {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+              Application.Quit();
+            }
+          }
+          // On Collision something something
+          void OnCollisionEnter(Collision collision) {
+            if (collision.gameObject.tag == "wall") {
 
-		if (Input.GetKey (KeyCode.R)) {
-			//print ("R is pressed");
-			this.transform.position = startposition.position;
-			this.transform.rotation = startposition.rotation;
-		}
-	 }
-		else
-	{
-		//print ("nothing is pressed");
-	}
+              print("Collision has been dectected");
+            }
+            if (collision.gameObject.tag == "Checkpoint") {
+              print("Checkpoint has been dectected");
+            }
+            if (collision.gameObject.tag == "start") {
+              print("start has been dectected");
+            }
+            if (collision.gameObject.tag == "end") {
+              print("end has been dectected");
+            }
+          }
+          void OnTriggerStay(Collider trigger) {
+            if (trigger.gameObject.tag == "Ladder") {
+              print("Ladder is enabled.");
+            }
+          }
 
-		//print ("Goodbye");
-}
-	void QuitApp() {
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			Application.Quit ();
+          void OnTriggerEnter(Collider other) {
 
-		}
-	}
-	//On Collision something something
-	void OnCollisionEnter(Collision collision) {
-		if (collision.gameObject.tag == "wall") {
+            if (other.gameObject.tag == "Pickup") {
+              print("Picked up object");
+              other.gameObject.SetActive(false);
+              count = count + 1;
+              SetCountText();
+            }
+            if (other.gameObject.tag == "Speedboost") {
+              print("SpeedBoost");
+              StartCoroutine(StopSpeedBoost());
+            }
+          }
+          void SetCountText() {
 
-			print ("Collision has been dectected");
+            countext.text = "Count" + count.ToString();
+            if (count >= wincount) {
+              wintext.text = "You win!";
+              Application.LoadLevel("level02");
+            }
+          }
 
-		}
-		if (collision.gameObject.tag == "Checkpoint") {
-			print ("Checkpoint has been dectected");
-		}
-		if (collision.gameObject.tag == "start") {
-			print ("start has been dectected");
-		}
-		if (collision.gameObject.tag == "end") {
-			print ("end has been dectected");
-
-		}
-
-	}
-	void OnTriggerStay(Collider trigger) {
-		if (trigger.gameObject.tag == "Ladder")
-		{
-			print ("Ladder is enabled.");
-
-		}
-}
-
-	void OnTriggerEnter(Collider other) {
-
-		if(other.gameObject.tag == "Pickup")
-		{
-			print ("Picked up object");
-
-			other.gameObject.SetActive (false);
-			count = count + 1;
-			SetCountText ();
-
-		}
-		if (other.gameObject.tag == "Speedboost")
-		{
-			print ("SpeedBoost");
-			StartCoroutine (StopSpeedBoost ());
-		}
-
-	}
-	void SetCountText() {
-
-		countext.text="Count" + count.ToString();
-		if(count >=wincount) {
-		wintext.text="You win!";
-		Application.LoadLevel("level02");
-
-		}
-
-	}
-
-	IEnumerator StopSpeedBoost(){
-		yield return  new WaitForSeconds (3F);
-		print ("speedboost over");
-
-	}
-
-
-}
+          IEnumerator StopSpeedBoost() {
+						speed=1.0F;
+						yield return new WaitForSeconds(3F);
+						speed=0.3;
+						print("speedboost over");
+          }
+  }
